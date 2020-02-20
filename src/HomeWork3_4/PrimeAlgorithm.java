848babee2077b45f3de7b1b8fde9c86edc816c92
Fadseres <evteev.mikhail.a@gmail.com>
2020-02-20 17:48:02 +0300

    решение
    думаем

--- src/HomeWork3_4/PrimeAlgorithm.java
+++ src/HomeWork3_4/PrimeAlgorithm.java
@@ -1,4 +1,22 @@
 package HomeWork3_4;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class PrimeAlgorithm {
+    public static void main(String[] args) {
+        System.out.println("Введите число от 0 до 100: ");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] era = new int[n];
+
+        for (int i = 0; i < n ; i++) {
+            System.out.println(Arrays.toString(era));
+        }
+
+
+    }
 }
